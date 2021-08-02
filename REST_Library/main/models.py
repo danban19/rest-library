bbf78f79176ee808bb97688b339aaadae8cb54a1
danban19 <daniel.banasik1@gmail.com>
2021-08-02 02:25:25 +0200

    final commit

--- REST_Library/main/models.py
+++ REST_Library/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
-    birth = models.CharField(max_length=100)
+    birth = models.IntegerField()
     create_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
 
--- REST_Library/main/serializers.py
+++ REST_Library/main/serializers.py
@@ -6,10 +6,8 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
         model = Author
         fields = ['url', 'name', 'surname', 'birth', 'create_date', 'edit_date']
 
-
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
         fields = ['url', 'name', 'author', 'create_date', 'edit_date']
-
 
